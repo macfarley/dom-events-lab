feb4db05e247cf8dc3c210426605a79a413378d0
macfarley90 <therealmccoyster@gmail.com>
2025-02-02 20:50:15 -0500

    completed work on the calculator with Paula B during support time

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const buttons = document.querySelectorAll('.button');
 const calculator = document.querySelector('#calculator');
 const display = document.querySelector('.display')
-
+const addStuff = '+'
 
 /*-------------------------------- Variables --------------------------------
 As a user, I want to be able to select numbers so that I can perform operations with them.
@@ -13,6 +13,12 @@ As a user, I want to be able to divide one number by another.
 As a user, I want to be able to see the output of the mathematical operation.
 As a user, I want to be able to clear all operations and start from 0.*/
 
+//I want an array where the current inputs push together into an algebra expression
+let displayMemory  = []
+let termOne = 0
+let operator = "a, b, c, d"
+let total = 0
+
 /*------------------------ Cached Element References ------------------------*/
 
 
@@ -20,25 +26,97 @@ As a user, I want to be able to clear all operations and start from 0.*/
 buttons.forEach((button) => {
   button.addEventListener('click', (event) => {
     // This log is for testing purposes to verify we're getting the correct value
-    console.log(event.target.innerText);
+    // console.log(event.target.innerText);
     // Future logic to capture the button's value would go here...
   });
 });
 calculator.addEventListener('click', (event) => {
   // This log is for testing purposes to verify we're getting the correct value
   // You have to click a button to see this log
-  console.log(event.target.innerText);
+  // console.log(event.target.innerText);
 
   // Example
   if (event.target.classList.contains('number')) {
     // show that number in the top display
-    document.display.textContent = `0 ${event.target.innerText}`; //what button was clicked
+    display.innerHTML = `${event.target.innerText}`; //what button was clicked
+    displayMemory.push(event.target.innerText); //stores the numbers
+    console.log(displayMemory); 
   }
 
-  // Example
-  if (event.target.innerText === '*') {
-    // Do something with this operator
+  // Equal sign = Add all the things in the displayMemory together and calculate
+  if (event.target.innerText === '=') {
+    let termTwo = displayMemory.join('')
+    displayMemory = []
+ 
+    //if i used + it adds
+    if(operator == 'addition'){
+      total = parseInt(termOne) + parseInt(termTwo)
+      console.log(total)
+    }
+
+    //if i used - it subtracts
+    if(operator == 'subtraction'){
+      total = parseInt(termOne) - parseInt(termTwo)
+      console.log(total)
+    }
+
+    //if i used / it divides
+    if(operator == 'division'){
+      total = parseInt(termOne) / parseInt(termTwo)
+      console.log(total)
+    }
+
+    //if i used * it multiplies
+    if(operator == 'multiplication'){
+      total = parseInt(termOne) * parseInt(termTwo)
+      console.log(total)
+    }
+    //Display the result, and save it in case they want to keep doing functions
+    display.innerHTML = `${total}`
+    displayMemory.push(total)
   }
+  
+  // Subtraction button
+  if (event.target.innerText === '-') {
+    console.log( displayMemory.join(''))
+    termOne = displayMemory.join('')
+    display.innerHTML = `${termOne}`
+    operator = 'subtraction'
+    displayMemory = []
+  }
+  // Addition button
+  if (event.target.innerText === '+') {
+    console.log( displayMemory.join(''))
+    termOne = displayMemory.join('')
+    display.innerHTML = `${termOne}`
+    operator = 'addition'
+    displayMemory = []
+  }
+  
+  // Divide button
+  if (event.target.innerText === '/') {
+    console.log( displayMemory.join(''))
+    termOne = displayMemory.join('')
+    display.innerHTML = `${termOne}`
+    operator = 'division'
+    displayMemory = []
+  }
+
+  //Multiplication button
+  if (event.target.innerText === '*') {
+    console.log( displayMemory.join(''))
+    termOne = displayMemory.join('')
+    display.innerHTML = `${termOne}`
+    operator = 'multiplication'
+    displayMemory = []
+  }
+
+  //Clear button
+  if (event.target.innerText === 'C') {
+    displayMemory = []
+    display.innerHTML = 0
+  }
+
 });
 
 
